1a6b280a89fe3b12d41ddd0493e1652892041f9a
Abby <ahmchua@gmail.com>
2018-10-23 11:16:19 -0500

    seq2seq model without attention, evaluation function is broken, but copy task works

---
+++ main.py
@@ -0,0 +1,115 @@
+import argparse
+import random
+import numpy as np
+import time
+import torch
+from torch import optim
+from lf_evaluator import *
+from models import *
+from data import *
+from utils import *
+from train import *
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='main.py')
+
+    # General system running and configuration options
+    parser.add_argument('--do_nearest_neighbor', dest='do_nearest_neighbor', default=False, action='store_true', help='run the nearest neighbor model')
+
+    parser.add_argument('--train_path', type=str, default='data/geo_train.tsv', help='path to train data')
+    parser.add_argument('--dev_path', type=str, default='data/geo_dev.tsv', help='path to dev data')
+    parser.add_argument('--test_path', type=str, default='data/geo_test.tsv', help='path to blind test data')
+    parser.add_argument('--test_output_path', type=str, default='geo_test_output.tsv', help='path to write blind test results')
+    parser.add_argument('--domain', type=str, default='geo', help='domain (geo for geoquery)')
+
+    # Some common arguments for your convenience
+    parser.add_argument('--seed', type=int, default=0, help='RNG seed (default = 0)')
+    parser.add_argument('--epochs', type=int, default=1, help='num epochs to train for')
+    parser.add_argument('--lr', type=float, default=.001)
+    parser.add_argument('--batch_size', type=int, default=2, help='batch size')
+    # 65 is all you need for GeoQuery
+    parser.add_argument('--decoder_len_limit', type=int, default=65, help='output length limit of the decoder')
+    parser.add_argument('--input_dim', type=int, default=100, help='input vector dimensionality')
+    parser.add_argument('--output_dim', type=int, default=100, help='output vector dimensionality')
+    parser.add_argument('--hidden_size', type=int, default=200, help='hidden state dimensionality')
+    parser.add_argument('--emb_dropout', type=int, default=0.2, help='Dropout for embedding layer')
+    parser.add_argument('--rnn_dropout', type=int, default=0, help='Dropout for LSTM')
+    parser.add_argument('--bidirectional', dest='bidirectional', default=False, action='store_true', help='run the nearest neighbor model')
+    parser.add_argument('--teacher_forcing_ratio', type=float, default=0.5)
+    args = parser.parse_args()
+    return args
+
+
+# Semantic parser that uses Jaccard similarity to find the most similar input example to a particular question and
+# returns the associated logical form.
+class NearestNeighborSemanticParser(object):
+    # Take any arguments necessary for parsing
+    def __init__(self, training_data):
+        self.training_data = training_data
+
+    # decode should return a list of k-best lists of Derivations. A Derivation consists of the underlying Example,
+    # a probability, and a tokenized output string. If you're just doing one-best decoding of example ex and you
+    # produce output y_tok, you can just return the k-best list [Derivation(ex, 1.0, y_tok)]
+    def decode(self, test_data):
+        # Find the highest word overlap with the test data
+        test_derivs = []
+        for test_ex in test_data:
+            test_words = test_ex.x_tok
+            best_jaccard = -1
+            best_train_ex = None
+            for train_ex in self.training_data:
+                # Compute word overlap
+                train_words = train_ex.x_tok
+                overlap = len(frozenset(train_words) & frozenset(test_words))
+                jaccard = overlap/float(len(frozenset(train_words) | frozenset(test_words)))
+                if jaccard > best_jaccard:
+                    best_jaccard = jaccard
+                    best_train_ex = train_ex
+            # N.B. a list!
+            print('best_train_ex.y_tok: ', best_train_ex.y_tok)
+            test_derivs.append([Derivation(test_ex, 1.0, best_train_ex.y_tok)])
+        return test_derivs
+
+
+if __name__ == '__main__':
+    args = _parse_args()
+    print(args)
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    # Load the training and test data
+
+    train, dev, test = load_datasets(args.train_path, args.dev_path, args.test_path, domain=args.domain)
+    train_data_indexed, dev_data_indexed, test_data_indexed, input_indexer, output_indexer = index_datasets(train, dev, test, args.decoder_len_limit)
+    print("%i train exs, %i dev exs, %i input types, %i output types" % (len(train_data_indexed), len(dev_data_indexed), len(input_indexer), len(output_indexer)))
+    print("Input indexer: %s" % input_indexer)
+    print("Output indexer: %s" % output_indexer)
+    print("Here are some examples post tokenization and indexing:")
+    for i in range(0, min(len(train_data_indexed), 10)):
+        print(train_data_indexed[i])
+    print(type(train_data_indexed[2]))
+    print(output_indexer.get_object(3))
+    if args.do_nearest_neighbor:
+        print("Doing nearest neighbor")
+        decoder = NearestNeighborSemanticParser(train_data_indexed)
+        test = decoder.decode(test_data_indexed[0:2])
+        print(test)
+        evaluate(dev_data_indexed, decoder)
+    else:
+        beam_length = 1
+        out = len(output_indexer)
+        train_data_indexed = train_data_indexed[0:10]
+        test_data_indexed  = test_data_indexed[0:1]
+        decoder = train_iters(train_data_indexed, args.epochs, input_indexer, output_indexer, args, beam_length, out)
+        # Next line tests copy act
+        #decoder = train_iters(train_data_indexed, args.epochs, input_indexer, input_indexer, args, beam_length, out)
+
+        pred = decoder.decode(test_data_indexed)
+        print("pred: ", pred)
+        #test = [(' '.join(d.y_toks)) for x in pred for d in x]
+        #for x in pred:
+            #for d in x:
+                #print("y_toks: ", d.y_toks)
+        #for i in test_data_indexed:
+            #print(i.y_tok)
+    print("=======FINAL EVALUATION=======")
+    evaluate(test_data_indexed, decoder, outfile="geo_test_output.tsv")
---
+++ train.py
@@ -0,0 +1,250 @@
+import argparse
+import random
+import numpy as np
+import time
+import torch
+from torch import optim
+from lf_evaluator import *
+from models import *
+from data import *
+from utils import *
+
+class Seq2SeqSemanticParser(object):
+    def __init__(self, model_input_emb, model_enc, model_output_emb, model_dec, args, output_indexer):
+        self.model_input_emb = model_input_emb
+        self.model_enc = model_enc
+        self.model_output_emb = model_output_emb
+        self.model_dec = model_dec
+        self.args = args
+        self.output_indexer = output_indexer
+        # Add any args you need here
+
+    def decode(self, test_data):
+        self.model_input_emb.eval()
+        self.model_enc.eval()
+        self.model_output_emb.eval()
+        self.model_dec.eval()
+
+        self.model_enc.zero_grad()
+        self.model_dec.zero_grad()
+
+        SOS_token = 1
+        EOS_token = 2
+        SOS_label = self.output_indexer.get_object(SOS_token)
+        beam_length = 1
+        derivations = []
+
+        for ex in test_data:
+            count = 0
+            y_toks =[SOS_label]
+            self.model_enc.zero_grad()
+            self.model_dec.zero_grad()
+
+            x_tensor = torch.as_tensor([ex.x_indexed])
+            y_tensor = torch.as_tensor(ex.y_indexed)
+            inp_lens_tensor = torch.as_tensor([ex.x_len()])
+
+            enc_output, enc_context_mask, enc_hidden = encode_input_for_decoder(x_tensor, inp_lens_tensor, self.model_input_emb, self.model_enc)
+            dec_hidden = enc_hidden
+            dec_input = torch.as_tensor([[SOS_token]])
+
+            while dec_input != EOS_token and count <= self.args.decoder_len_limit:
+                dec_output, dec_input, dec_input_val, dec_hidden = decode(dec_input, dec_hidden, self.model_output_emb, self.model_dec, beam_length)
+                #print("dec_output: ", dec_output)
+                y_label = self.output_indexer.get_object(int(dec_input.squeeze()))
+                y_toks.append(y_label)
+                #y_toks.append(int(dec_input.squeeze()))
+                count = count + 1
+            derivations.append([Derivation(ex, 1.0 , str(y_toks))])
+            print("prediction: ", y_toks)
+        return derivations
+
+
+# Takes the given Examples and their input indexer and turns them into a numpy array by padding them out to max_len.
+# Optionally reverses them.
+def make_padded_input_tensor(exs, input_indexer, max_len, reverse_input):
+    if reverse_input:
+        return np.array(
+            [[ex.x_indexed[len(ex.x_indexed) - 1 - i] if i < len(ex.x_indexed) else input_indexer.index_of(PAD_SYMBOL)
+              for i in range(0, max_len)]
+             for ex in exs])
+    else:
+        return np.array([[ex.x_indexed[i] if i < len(ex.x_indexed) else input_indexer.index_of(PAD_SYMBOL)
+                          for i in range(0, max_len)]
+                         for ex in exs])
+
+# Analogous to make_padded_input_tensor, but without the option to reverse input
+def make_padded_output_tensor(exs, output_indexer, max_len):
+    return np.array([[ex.y_indexed[i] if i < len(ex.y_indexed) else output_indexer.index_of(PAD_SYMBOL) for i in range(0, max_len)] for ex in exs])
+
+
+# Runs the encoder (input embedding layer and encoder as two separate modules) on a tensor of inputs x_tensor with
+# inp_lens_tensor lengths.
+# x_tensor: batch size x sent len tensor of input token indices
+# inp_lens: batch size length vector containing the length of each sentence in the batch
+# model_input_emb: EmbeddingLayer
+# model_enc: RNNEncoder
+# Returns the encoder outputs (per word), the encoder context mask (matrix of 1s and 0s reflecting
+
+# E.g., calling this with x_tensor (0 is pad token):
+# [[12, 25, 0, 0],
+#  [1, 2, 3, 0],
+#  [2, 0, 0, 0]]
+# inp_lens = [2, 3, 1]
+# will return outputs with the following shape:
+# enc_output_each_word = 3 x 4 x dim, enc_context_mask = [[1, 1, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0]],
+# enc_final_states = 3 x dim
+def encode_input_for_decoder(x_tensor, inp_lens_tensor, model_input_emb, model_enc):
+    input_emb = model_input_emb.forward(x_tensor)
+    #print("input_emb: ", input_emb)
+    (enc_output_each_word, enc_context_mask, enc_final_states) = model_enc.forward(input_emb, inp_lens_tensor)
+    enc_final_states_reshaped = (enc_final_states[0].unsqueeze(0), enc_final_states[1].unsqueeze(0))
+    #enc_final_states_reshaped = (enc_final_states[0].squeeze(), enc_final_states[1].squeeze())
+    return (enc_output_each_word, enc_context_mask, enc_final_states_reshaped)
+
+
+def decode(y_index, hidden, model_output_emb, model_dec, beam_length):
+    #print("y_ex: ", y_index)
+    output_emb = model_output_emb.forward(y_index)
+    #print("output_emb: ", output_emb)
+    output, hidden = model_dec.forward(output_emb, hidden)
+    top_val, top_ind = output.topk(beam_length)
+    dec_input = top_ind.detach()
+    dec_input_prob = top_val.detach()
+    return output, dec_input, dec_input_prob, hidden
+
+def train_model_encdec(ex, model_input_emb, model_enc, model_output_emb, model_dec, enc_optimizer, dec_optimizer, beam_length, teacher_forcing_ratio):
+    # Sort in descending order by x_indexed, essential for pack_padded_sequence
+    #train_data.sort(key=lambda ex: len(ex.x_indexed), reverse=True)
+    #test_data.sort(key=lambda ex: len(ex.x_indexed), reverse=True)
+
+    # Create indexed input
+    #input_max_len = np.max(np.asarray([len(ex.x_indexed) for ex in train_data]))
+    #all_train_input_data = make_padded_input_tensor(train_data, input_indexer, input_max_len, args.reverse_input)
+    #all_test_input_data = make_padded_input_tensor(test_data, input_indexer, input_max_len, args.reverse_input)
+
+    #output_max_len = np.max(np.asarray([len(ex.y_indexed) for ex in train_data]))
+    #all_train_output_data = make_padded_output_tensor(train_data, output_indexer, output_max_len)
+    #all_test_output_data = make_padded_output_tensor(test_data, output_indexer, output_max_len)
+
+    #print("Train length: %i" % input_max_len)
+    #print("Train output length: %i" % np.max(np.asarray([len(ex.y_indexed) for ex in train_data])))
+    #print("Train matrix: %s; shape = %s" % (all_train_input_data, all_train_input_data.shape))
+
+    # Initialize loss
+    loss = 0
+    SOS_token = 1
+    EOS_token = 2
+    criterion = torch.nn.NLLLoss()
+
+    # Loop over epochs, loop over examples, given some indexed words, call encode_input_for_decoder, then call your
+    # decoder, accumulate losses, update parameters
+
+    model_enc.zero_grad()
+    model_dec.zero_grad()
+
+    x_tensor = torch.as_tensor([ex.x_indexed])
+    #y_tensor = torch.as_tensor(ex.x_indexed)
+    y_tensor = torch.as_tensor(ex.y_indexed)
+    inp_lens_tensor = torch.as_tensor([ex.x_len()])
+    enc_output, enc_context_mask, enc_hidden = encode_input_for_decoder(x_tensor, inp_lens_tensor, model_input_emb, model_enc)
+    dec_hidden = enc_hidden
+
+    #print("enc_hidden shape", enc_hidden)
+
+    dec_input = torch.as_tensor([[SOS_token]])
+    y_temp = []
+
+    teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    for y_ex in range(len(y_tensor)):
+        dec_output, dec_input, dec_input_val, dec_hidden = decode(dec_input, dec_hidden, model_output_emb, model_dec, beam_length)
+        loss += criterion(dec_output, y_tensor[y_ex].unsqueeze(0))
+        y_temp.append(int(dec_input.squeeze()))
+        if dec_input ==EOS_token:
+            break
+        if teacher_forcing:
+            dec_input = y_tensor[y_ex].unsqueeze(0).unsqueeze(0)
+
+    print("predicted: ", y_temp)
+    print("actual: ", ex.x_indexed, '\n')
+
+    loss.backward()
+    enc_optimizer.step()
+    dec_optimizer.step()
+
+    return loss
+
+def train_iters(train_data, epochs, input_indexer, output_indexer, args, beam_length, out):
+
+    # Create encoder, decoder and embedding layers
+    model_input_emb = EmbeddingLayer(args.input_dim, len(input_indexer), args.emb_dropout)
+    model_enc = RNNEncoder(args.input_dim, args.hidden_size, args.rnn_dropout, args.bidirectional)
+    model_output_emb = EmbeddingLayer(args.input_dim, len(output_indexer), args.emb_dropout)
+    model_dec = RNNDecoder(args.input_dim, args.hidden_size, out, dropout = 0 )
+
+    #Initialize optimizers
+    enc_optimizer = optim.Adam(model_enc.parameters(), lr=args.lr)
+    dec_optimizer = optim.Adam(model_dec.parameters(), lr=args.lr)
+
+    count = 0.0
+    for i in range(epochs):
+        loss = 0.0
+        for ex in train_data:
+            inc_loss = train_model_encdec(ex, model_input_emb, model_enc, model_output_emb, model_dec, enc_optimizer, dec_optimizer, beam_length, args.teacher_forcing_ratio)
+            if count %25 ==0:
+                print ("loss: ", inc_loss)
+            count = count + 1
+
+        print("epoch: ", i)
+        print("loss: ", loss)
+
+    return Seq2SeqSemanticParser(model_input_emb, model_enc, model_output_emb, model_dec, args, output_indexer)
+
+
+# Evaluates decoder against the data in test_data (could be dev data or test data). Prints some output
+# every example_freq examples. Writes predictions to outfile if defined. Evaluation requires
+# executing the model's predictions against the knowledge base. We pick the highest-scoring derivation for each
+# example with a valid denotation (if you've provided more than one).
+def evaluate(test_data, decoder, example_freq=50, outfile=None):
+    e = GeoqueryDomain()
+    pred_derivations = decoder.decode(test_data)
+    #print("\n pred_derivations: ", pred_derivations, '\n')
+    selected_derivs, denotation_correct = e.compare_answers([ex.y for ex in test_data], pred_derivations)
+    num_exact_match = 0
+    num_tokens_correct = 0
+    num_denotation_match = 0
+    total_tokens = 0
+    #print("selected_derivs_len: ", len(selected_derivs))
+    for i, ex in enumerate(test_data):
+        #print("i: ", i)
+        #print(selected_derivs[i])
+        if i % example_freq == 0:
+            print('Example %d' % i)
+            print('  x      = "%s"' % ex.x)
+            print('  y_tok  = "%s"' % ex.y_tok)
+            print('  y_pred = "%s"' % selected_derivs[i].y_toks)
+        # Compute accuracy metrics
+        y_pred = ' '.join(selected_derivs[i].y_toks)
+        # Check exact match
+        if y_pred == ' '.join(ex.y_tok):
+            num_exact_match += 1
+        # Check position-by-position token correctness
+        num_tokens_correct += sum(a == b for a, b in zip(selected_derivs[i].y_toks, ex.y_tok))
+        total_tokens += len(ex.y_tok)
+        # Check correctness of the denotation
+        if denotation_correct[i]:
+            num_denotation_match += 1
+    print("Exact logical form matches: %s" % (render_ratio(num_exact_match, len(test_data))))
+    print("Token-level accuracy: %s" % (render_ratio(num_tokens_correct, total_tokens)))
+    print("Denotation matches: %s" % (render_ratio(num_denotation_match, len(test_data))))
+    # Writes to the output file if needed
+    if outfile is not None:
+        with open(outfile, "w") as out:
+            for i, ex in enumerate(test_data):
+                out.write(ex.x + "\t" + " ".join(selected_derivs[i].y_toks) + "\n")
+        out.close()
+
+
+def render_ratio(numer, denom):
+    return "%i / %i = %.3f" % (numer, denom, float(numer)/denom)
